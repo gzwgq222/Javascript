4148dd81f81e3174d3ad812d41fe531c9ef1324e
gzwgq222 <445722156@qq.com>
2019-09-02 18:38:42 +0800

    feat：利用对象 key 唯一性进行数组的去重

--- unique.js
+++ unique.js
@@ -1,5 +1,5 @@
 /* 利用对象key唯一性去重 */
-
+// 元素作 key 和 vlue，获取所有的 value
 function unique(arr) {
     let map = {};
     arr.forEach(item => {
@@ -7,9 +7,23 @@ function unique(arr) {
     })
     return [...Object.values(map)]; // keys 获得的是字符
 }
+// 元素作 key 并设置状态
+function unique1(arr) {
+    let map = {},
+        newArr = [];
+    arr.forEach(item => {
+        if (!map[item]) {
+            map[item] = true;
+            newArr.push(item);
+        }
+    })
+    return newArr;
+}
 
 const arr = [1, 2, 1, 3, 5, 2];
+
 console.log(unique(arr));
+console.log('unique1: ', unique1(arr));
 
 /* 查找字符中重复次数最多的字母 */
 
